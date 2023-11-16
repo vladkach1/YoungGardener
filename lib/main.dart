265622af499884dc8a7814a700ae6743af1f4298
Rodjolo <pryanikov.rodion@mail.ru>
2023-11-16 19:58:37 +0300

    Adding appBar with design and plant with description text

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
@@ -8,38 +10,27 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          toolbarHeight: 100,
-          title: Text('Главная'),
-          flexibleSpace: Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: [Color.fromARGB(255, 168, 209, 161),Color.fromARGB(255, 136, 207, 123)], // Здесь можно задать цвета градиента
-              ),
-            ),
-          ),
+      home: MyHomePage(title: 'Главная'),
+      routes: {
+        '/plantInfo': (context) => PlantInfoScreen(),
+      },
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          elevation: 0,
         ),
-        body: Center(
-          child: Text(
-            '',
-            style: TextStyle(fontSize: 20),
-          ),
-        ),
+        scaffoldBackgroundColor: Color.fromARGB(255, 237, 232, 232),
       ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
 
   @override
@@ -62,13 +53,6 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void initialization() async {
-    print('ready in 3...');
-    await Future.delayed(const Duration(seconds: 1));
-    print('ready in 2...');
-    await Future.delayed(const Duration(seconds: 1));
-    print('ready in 1...');
-    await Future.delayed(const Duration(seconds: 1));
-    print('go!');
     FlutterNativeSplash.remove();
   }
 
@@ -76,8 +60,8 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
+        flexibleSpace: appBarGradient(),
       ),
       body: Center(
         child: Column(
@@ -88,7 +72,14 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             Text(
               '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/plantInfo');
+              },
+              child: Text('О растении'),
             ),
           ],
         ),
@@ -97,7 +88,159 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), 
+      ),
+    );
+  }
+}
+
+class PlantInfoScreen extends StatelessWidget {
+  final TextStyle textDescriptionStyle = GoogleFonts.inter(fontWeight: FontWeight.bold);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 25),
+              child: SvgPicture.asset(
+                'assets/icons/plant.svg',
+                width: 35,
+                height: 35,
+              ),
+            ),
+            SizedBox(width: 10),
+            Padding(
+              padding: const EdgeInsets.only(top: 17),
+              child: Text(
+                'Информация о растении',
+                style: GoogleFonts.inika(fontSize: 16, color: Colors.black),
+              ),
+            ),
+            SizedBox(width: 40),
+            Padding(
+              padding: const EdgeInsets.only(top: 25),
+              child: SvgPicture.asset(
+                'assets/icons/tree.svg',
+                width: 35,
+                height: 35,
+              ),
+            ),
+          ],
+        ),
+        toolbarHeight: 65,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(30),
+          ),
+        ),
+        leading: Padding(
+          padding: const EdgeInsets.only(top: 17),
+          child: IconButton(
+            icon: SvgPicture.asset('assets/icons/back_arrow.svg'),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ),
+        flexibleSpace: appBarGradient(),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(40),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Image.asset(
+                  'assets/images/plant.png',
+                  width: 129,
+                  height: 132,
+                ),
+                SizedBox(width: 20),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Sunshine',
+                      style: textDescriptionStyle,
+                    ),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.wb_sunny,
+                          color: Color.fromARGB(255, 168, 209, 161),
+                        ),
+                        SizedBox(width: 15),
+                        Text(
+                          '16-18°C',
+                          style: textDescriptionStyle,
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 2),
+                    Text(
+                      'Humidity',
+                      style: textDescriptionStyle,
+                    ),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.opacity,
+                          color: Color.fromARGB(255, 168, 209, 161),
+                        ),
+                        SizedBox(width: 15),
+                        Text(
+                          '80-85%',
+                          style: textDescriptionStyle,
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 2),
+                    Text(
+                      'Size',
+                      style: textDescriptionStyle,
+                    ),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.straighten,
+                          color: Color.fromARGB(255, 168, 209, 161),
+                        ),
+                        SizedBox(width: 15),
+                        Text(
+                          '60-90 cm',
+                          style: textDescriptionStyle,
+                        ),
+                      ],
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class appBarGradient extends StatelessWidget {
+  const appBarGradient({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+            Color.fromARGB(255, 168, 209, 161),
+            Color.fromARGB(255, 136, 207, 123),
+          ],
+        ),
+      ),
     );
   }
 }
