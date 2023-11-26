a0a9d30ce26d133a3fca481a0bbc242f241d63dc
vladkach1 <76248790+vladkach1@users.noreply.github.com>
2023-11-26 19:37:56 +0300

    Create main-screen

---
+++ lib/main-screen.dart
@@ -0,0 +1,298 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(
+        title: 'Главная',
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  String _namePlans(int i) {
+    List<String> Plans = [' Петрушка кудрявая', ' Алоэ вера'];
+    return Plans[i];
+  }
+
+  Widget _remindList() {
+    return SliverToBoxAdapter(
+      child: Padding(
+        padding: const EdgeInsets.only(left: 35),
+        child: Column(
+          children: [
+            //Напоминание 1
+            Slidable(
+              endActionPane: ActionPane(
+                extentRatio: 0.6,
+                closeThreshold: 0.9,
+                motion: BehindMotion(),
+                children: [
+                  Container(
+                    child: SvgPicture.asset('assets/icons/checkmark.svg'),
+                    padding: EdgeInsets.only(
+                        top: 20, bottom: 20, left: 9, right: 10),
+                    decoration: BoxDecoration(
+                      color: Color(0xff54AD45),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                ],
+              ),
+              child: Row(
+                children: [
+                  Container(
+                    padding:
+                    EdgeInsets.symmetric(vertical: 5, horizontal: 33),
+                    margin: EdgeInsets.only(right: 20),
+                    decoration: BoxDecoration(
+                      color: Color(0xffC7C4C4),
+                      borderRadius: BorderRadius.circular(12),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 2,
+                          blurRadius: 5,
+                          offset: Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    child: Row(
+                      children: [
+                        Column(
+                          children: [
+                            SizedBox(
+                              height: 10,
+                            ),
+                            SvgPicture.asset('assets/icons/plant2.svg'),
+                          ],
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Петрушка кудрявая',
+                              style: GoogleFonts.inika(
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              'Растение нужно полить (150мл)',
+                              style: GoogleFonts.inter(fontSize: 10),
+                            )
+                          ],
+                        ),
+                        SizedBox(width: 53),
+                      ],
+                    ),
+                  ),
+                  // Switch(
+                  //   thumbIcon: thumbIcon,
+                  //   value: light1,
+                  //   onChanged: (bool value) {
+                  //     setState(() {
+                  //       light1 = value;
+                  //     });
+                  //   },
+                  // ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+          ],
+        ),
+      ),
+    );}
+
+  Widget _remind() {
+    return SliverList.builder(
+      itemBuilder: (context, index) => Container(
+        height: 60,
+        margin: EdgeInsets.only(left: 35, top: 10, right: 20),
+        child: Row(
+          children: [
+            SvgPicture.asset('assets/icons/notification.svg'),
+            SizedBox(
+              width: 10,
+            ),
+            Text(
+              'Напоминания',
+              style: GoogleFonts.inter(fontSize: 20),
+            ),
+          ],
+        ),
+      ),
+      itemCount: 1,
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initialization();
+  }
+
+  void initialization() async {
+    print('ready in 3...');
+    await Future.delayed(const Duration(seconds: 1));
+    print('ready in 2...');
+    await Future.delayed(const Duration(seconds: 1));
+    print('ready in 1...');
+    await Future.delayed(const Duration(seconds: 1));
+    print('go!');
+    FlutterNativeSplash.remove();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(26),
+                bottomRight: Radius.circular(26)),
+            gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  Color.fromARGB(255, 168, 209, 161),
+                  Color.fromARGB(255, 136, 207, 123)
+                ]),
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 10, top: 20),
+            child: IconButton(
+              color: Colors.black,
+              onPressed: _incrementCounter,
+              icon: Icon(Icons.add),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 140, right: 10),
+            child: ImageIcon(
+              AssetImage('assets/icons/tree.png'),
+              color: Colors.black,
+              size: 35,
+            ),
+          ),
+        ],
+        toolbarHeight: 200,
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(50.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Ваши растения',
+                style: GoogleFonts.inder(
+                  fontSize: 22,
+                  color: Colors.black,
+                ),
+              ),
+            ],
+          ),
+        ),
+        title: Padding(
+          padding: const EdgeInsets.only(top: 30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Здравтсвуйте,\nUserName!',
+                style: GoogleFonts.inika(
+                  fontSize: 30,
+                  color: Colors.black,
+                ),
+              ),
+              Text(
+                'Взгляните на Ваши растения',
+                style: GoogleFonts.inder(
+                  fontSize: 16,
+                  color: Colors.black,
+                ),
+              ),
+            ],
+          ),
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(30),
+          ),
+        ),
+      ),
+      body: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[],
+            ),
+          ),
+          SliverList.builder(
+            itemBuilder: (context, index) => Container(
+              height: 53,
+              margin: EdgeInsets.only(left: 35, top: 10, right: 35),
+              child: ElevatedButton(
+                onPressed: _incrementCounter,
+                style: TextButton.styleFrom(
+                  backgroundColor: Color(0xffC7C4C4),
+                  primary: Colors.green,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+                child: Align(
+                  alignment: Alignment(-1, 0),
+                  child: Text(
+                    (index + 1).toString() + ' ' + _namePlans(index),
+                    style: GoogleFonts.inder(
+                      fontSize: 18,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            itemCount: 2,
+          ),
+          _remind(),
+          _remindList(),
+        ],
+      ),
+      //padding: EdgeInsets.all(50),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:young_gardener/main-screen.dart';
 
 void main() {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
@@ -7,77 +11,3 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MyHomePage(title: 'Главная',),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    initialization();
-  }
-
-  void initialization() async {
-    print('ready in 3...');
-    await Future.delayed(const Duration(seconds: 1));
-    print('ready in 2...');
-    await Future.delayed(const Duration(seconds: 1));
-    print('ready in 1...');
-    await Future.delayed(const Duration(seconds: 1));
-    print('go!');
-    FlutterNativeSplash.remove();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
-    );
-  }
-}
